f9c4e6e8899c9c6620afc8af011f204bf6f22849
Mohd Imran <m.imran8811@gmail.com>
2024-06-15 20:20:28 +0500

    changes

--- dist/main.js
+++ dist/main.js
@@ -5,7 +5,12 @@ const app_module_1 = require("./app/app.module");
 const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors();
+    app.enableCors({
+        origin: ['http://localhost:3000'],
+        methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+        credentials: true,
+        allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+    });
     app.useGlobalPipes(new common_1.ValidationPipe());
     await app.listen(3000);
 }
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  // app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+    credentials: true,
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
