16b1b11cd4ed111e9dd07105a83e56fe12c60e8c
Mohd Imran <m.imran8811@gmail.com>
2024-06-16 12:43:59 +0500

    changes

--- dist/main.js
+++ dist/main.js
@@ -5,12 +5,7 @@ const app_module_1 = require("./app/app.module");
 const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors({
-        credentials: true,
-        origin: ['http://localhost:3000'],
-        methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
-        allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
-    });
+    app.enableCors();
     app.useGlobalPipes(new common_1.ValidationPipe());
     await app.listen(3000);
 }
