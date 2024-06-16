472a626fb75b282ed5e9ad1c1c15e1a29772861d
Mohd Imran <m.imran8811@gmail.com>
2024-06-16 15:11:30 +0500

    changes

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "http://localhost:3000"
+    origin: "http://localhost:3000, https://www.pkapparel.com"
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
