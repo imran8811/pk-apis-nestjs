44d02fdf5a58cc044225e14704d2217506a114f6
Mohd Imran <m.imran8811@gmail.com>
2024-06-16 15:19:23 +0500

    changes

--- dist/main.js
+++ dist/main.js
@@ -6,7 +6,7 @@ const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        origin: "http://localhost:3000"
+        origin: "https://www.pkapparel.com"
     });
     app.useGlobalPipes(new common_1.ValidationPipe());
     await app.listen(3000);
